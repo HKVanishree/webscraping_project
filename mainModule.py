13e7fb26a209827d3fd2dfb9195558e326acc95a
Vanishree Hu <vanishree.hu@hashedin.com>
2019-04-23 15:18:10 +0530

    Errors handled for config file

--- mainModule.py
+++ mainModule.py
@@ -2,9 +2,12 @@ import configparser
 from webScrapping import scrapping
 
 def main():
-    config = configparser.ConfigParser()
-    config.read('ConfigFile.ini')
-    print("Inside main")
+    try:
+     config = configparser.ConfigParser()
+     config.read('ConfigFile.ini')
+    except Exception as e:
+        print("Manual errors found in the configuration file\n")
+        exit(1)
     keyWordsList = config.get('KEYWORD', 'k1')
     keyWords = keyWordsList.split(",")
 
