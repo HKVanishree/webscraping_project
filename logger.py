533238fd9018818b6aac7510fd79d7ec77cb2fe9
Vanishree Hu <vanishree.hu@hashedin.com>
2019-04-23 15:14:00 +0530

    Separate modules for config parser and logger created

---
+++ logger.py
@@ -0,0 +1,24 @@
+from logging.handlers import RotatingFileHandler
+import configparser
+import logging
+
+
+class loggerModule:
+  def logging(self):
+    config = configparser.ConfigParser()
+    config.read('ConfigFile.ini')
+
+    LogLevel = config.get('LOGLEVEL', 'log')
+    logging.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s', level=logging.INFO)
+    logger = logging.getLogger('example_logger')
+    Rthandler = RotatingFileHandler('log/webScrap.log', maxBytes=500, backupCount=5)
+
+    if LogLevel == 'debug':
+        logger.setLevel(logging.DEBUG)
+    if LogLevel == 'info':
+        logger.setLevel(logging.INFO)
+    if LogLevel == 'error':
+        logger.setLevel(logging.ERROR)
+    if LogLevel == 'critical':
+        logger.setLevel(logging.CRITICAL)
+
